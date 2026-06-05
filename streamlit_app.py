68325b334a98544af8917b5e5119409283b8d1ab
Maksim Poltoradnev <66369466+max-poltora@users.noreply.github.com>
2026-06-05 12:35:50 +0000

    update 1

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,26 +12,27 @@ my_container = st.container(border=True)
 col1, col2 = my_container.columns([0.8, 0.2])
 
 with col1:
-    team1 = st.selectbox(
+    team1_name = st.selectbox(
         'Select the team 1',
         options=TEAMS,
         index=None
     )
+
+    team2_name = st.selectbox(
+        'Select the team 2',
+        options=TEAMS,
+        index=None
+    )
+
+
 with col2:
-    score_team1 = st.number_input(key='score_team1', 
+    team1_score = st.number_input(key='team1_score', 
                                     label='',
                                     min_value=0,
                                     step=1
                                     )
     
-with col1:
-    team2 = st.selectbox(
-        'Select the team 2',
-        options=TEAMS,
-        index=None
-    )
-with col2:
-    score_team2 = st.number_input(key='score_team2', 
+    team2_score = st.number_input(key='team2_score', 
                                     label='',
                                     min_value=0,
                                     step=1
